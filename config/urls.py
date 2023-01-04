27c15959e03e831e09580caff8ff6eb13979856b
Olivier Corradi <olivier.corradi@gmail.com>
2023-01-05 00:15:56 +0100

    Add django-allauth

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("mainapp.urls")),
-    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("allauth.urls")),
+    path("logout", LogoutView.as_view()),
 ]
