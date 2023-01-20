eef783a1fdcd0afd4ad737784f019e1ddc920718
Olivier Corradi <olivier.corradi@gmail.com>
2023-01-20 21:25:30 +0100

    More verbose task errors

--- mainapp/tasks.py
+++ mainapp/tasks.py
@@ -1,9 +1,11 @@
 import socket
+from pprint import pformat
 
 from celery import shared_task
 from celery.signals import task_failure
 from celery.utils.log import get_task_logger
 from django.core.mail import mail_admins
+from django.forms.models import model_to_dict
 
 from .models import Measurement, Metric
 
@@ -31,10 +33,15 @@ def collect_all_latest_task():
 
 
 @task_failure.connect()
-def celery_task_failure_email(**kwargs):
+def celery_task_failure_email(sender, *args, **kwargs):
     """celery 4.0 onward has no method to send emails on failed tasks
     so this event handler is intended to replace it
     """
+    extras = {}
+
+    if sender == collect_latest_task:
+        metric = Metric.objects.get(pk=args[0])
+        extras = model_to_dict(metric)
 
     subject = "[Django][{queue_name}@{host}] Error: Task {sender.name} ({task_id}): {exception}".format(
         queue_name="celery",  # `sender.queue` doesn't exist in 4.1?
@@ -45,14 +52,16 @@ def celery_task_failure_email(**kwargs):
     message = """Task {sender.name} with id {task_id} raised exception:
 {exception!r}
 
+args = {args}
+kwargs = {kwargs}
 
-Task was called with args: {args} kwargs: {kwargs}.
+extras = {extras}
 
 The contents of the full traceback was:
 
 {einfo}
     """.format(
-        **kwargs
+        sender=sender, extras=pformat(extras), *args, **kwargs
     )
 
     mail_admins(subject, message)
