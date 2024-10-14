331704ecea05e095a769b3f5136b7d5d222a0ec9
madsnedergaard <nedergaardmads@gmail.com>
2024-10-14 14:20:24 +0200

    Fixes seed

--- seed.py
+++ seed.py
@@ -1,5 +1,5 @@
 from mainapp.models import Metric, User
 
-admin_user = User.objects.get(username="admin")
+admin_user = User.objects.get(username="john")
 Metric(integration_id="plausible", name="test_metric", user=admin_user).save()
 Metric(integration_id="postgresql", name="test_metric_sql", user=admin_user).save()
