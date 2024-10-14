466d61b5b2df2117d16e737de54905c0a99905ea
madsnedergaard <nedergaardmads@gmail.com>
2024-10-14 14:26:33 +0200

    Makes seed usable by creating a dashboard and adding metrics

--- seed.py
+++ seed.py
@@ -1,5 +1,19 @@
-from mainapp.models import Metric, User
+from mainapp.models import Dashboard, Metric, User
 
 admin_user = User.objects.get(username="john")
-Metric(integration_id="plausible", name="test_metric", user=admin_user).save()
-Metric(integration_id="postgresql", name="test_metric_sql", user=admin_user).save()
+plausible_metric = Metric(
+    integration_id="plausible", name="test_metric", user=admin_user
+)
+plausible_metric.save()
+postgresql_metric = Metric(
+    integration_id="postgresql", name="test_metric_sql", user=admin_user
+)
+postgresql_metric.save()
+dashboard = Dashboard(
+    slug="test_dashboard",
+    is_public=False,
+    name="test_dashboard",
+    user=admin_user,
+)
+dashboard.save()
+dashboard.metrics.set([plausible_metric, postgresql_metric])
